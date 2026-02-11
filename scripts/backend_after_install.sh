c1ec0b3285273d0026e9f08caf6bf016b90335a9
Vanshaj <srivastavanshaj10j@gmail.com>
2026-02-11 10:07:36 +0000

    after_install changes

--- scripts/backend_after_install.sh
+++ scripts/backend_after_install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+sudo chown -R ec2-user:ec2-user /opt/backend
 cd /opt/backend
+export PATH="/usr/local/bin:/usr/bin:/bin:$PATH"
+
 if [ -f package-lock.json ]; then
   npm ci --omit=dev
 else
